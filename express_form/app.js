1920eb5ccf0bc9e0209eb3adbc694e1eebabf346
cg-dv <cgdevproject@gmail.com>
2021-08-20 15:51:13 -0700

    Removed old node server code, used Express server code instead, removed extraneous imports from Express example code

--- express_form/app.js
+++ express_form/app.js
@@ -1,42 +1,5 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 // Use this code snippet in your app.
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
@@ -116,26 +79,17 @@ client.getSecretValue({SecretId: secretName}, function(err, data) {
 
       // Write MySQL data to file, set up app server to serve response containing JSON data from MySQL
 
-      //const express = require('express')
-      //const app = express()
-      const port = 3000
+      app.set('port', (process.env.PORT || 8080))
+      app.use(express.static(__dirname + '/public'))
 
-      app.get('/', (req, res) => {
-        fs = require('fs');
-        fs.writeFile('data.txt', response_text, function (err) {
-          if (err) return console.log(err);
-          console.log('Output file written.\n');
-        })
-      })
-      
-      app.get('/', (req, res) => {
-        res.sendFile('data.txt', { root: '.' })
+      app.get('/', function(request, response) {
+        response.send(response_text)
       })
 
-      app.listen(port, () => {
-        console.log(`App listening at http://localhost:${port}`)
+      app.listen(app.get('port'), function() {
+        console.log("App listening at localhost:" + app.get('port'))
       })
-    
+
     });
 
     connection.end();
